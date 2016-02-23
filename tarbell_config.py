42301ce64a26214d9e44d3764790bb9b5a8e4f15
gurmanbh <gurmanbhatia.93@gmail.com>
2016-02-22 22:27:41 -0500

    mostly everything works. need to fix question w/o y/n

--- tarbell_config.py
+++ tarbell_config.py
@@ -14,7 +14,7 @@ EXCLUDES = ["*.md", "requirements.txt"]
 # SPREADSHEET_CACHE_TTL = 4
 
 # Create JSON data at ./data.json, disabled by default
-CREATE_JSON = True
+# CREATE_JSON = True
 
 # Get context from a local file or URL. This file can be a CSV or Excel
 # spreadsheet file. Relative, absolute, and remote (http/https) paths can be 
@@ -42,14 +42,9 @@ DEFAULT_CONTEXT = {
 }
 
 from flask import Blueprint, g, render_template,Flask, url_for
+from flask_frozen import Freezer
 from jinja2 import evalcontextfilter, contextfunction, Template, Markup
 from tarbell.hooks import register_hook
-
-# @my_blueprint.route('/foo/')
-# def foo():
-    # site = g.current_site
-#     return site.data['values']['headline']
-# @eads
 
 def build_dict(seq, key):
 	return dict((d[key], dict(d, index=index)) for (index, d) in enumerate(seq))
@@ -61,23 +56,94 @@ def build_competition(dictlist,city, seat, name):
     expectedResult = [d for d in dictlist if d['city'] == city and d['seat']==seat and d['namekey']!=name ]
     return expectedResult
 
+def build_questions(list,city):
+    masterdict = {}
+    for item in list:
+        city = item["city"]
+        if city not in masterdict:
+            masterdict[city] = []
+        masterdict[city].append(item.copy())
+    return masterdict
+
+def buildcityseatlist(list):
+    masterdict = {}
+    for item in list:
+        city = item["city"]
+        namekey = item["namekey"]
+        seat = item["seat"]
+        if city not in masterdict:
+            masterdict[city] = {}
+        if seat not in masterdict[city]:
+            masterdict[city][seat] = []
+        temp = {}
+        temp["status"] = item["status"]
+        temp["name"] = item["name"]
+        temp["cleanname"] = item["cleanname"]
+        temp["city"] = item["city"]
+        temp["namekey"] = item["namekey"]
+        masterdict[city][seat].append(temp.copy())
+    return masterdict
+
+def getraces(selected):
+    obj = []
+    if 'List_elected_offices_you_have_held_and_date_ranges_for_each_office' in selected:
+        wonraces = selected['List_elected_offices_you_have_held_and_date_ranges_for_each_office'].split('|')
+        for race in wonraces:
+            check = {}
+            if "(" in race:
+                check['race'] = race.split(' (')[0]
+                if "-" in race:
+                    check['startyear'] = race.split(' (')[1].split('-')[0].strip()
+                    check['endyear'] = check['orderyear'] = race.split(' (')[1].split('-')[1].replace (")", "").strip()
+                else:
+                    check['startyear'] = race.split(' (')[1].replace (")", "").strip()
+                    check['endyear'] = check['orderyear'] = check['startyear']
+            check['type'] = 'won'
+            check['racestring'] = race
+            obj.append(check.copy())
+    if 'List_positions_and_years_you_unsuccessfully_ran_for_office' in selected:
+        lostraces = selected['List_positions_and_years_you_unsuccessfully_ran_for_office'].split('|')
+        for race in lostraces:
+            check = {}
+            if "(" in race:
+                check['race'] = race.split(' (')[0]
+                check['startyear'] = race.split(' (')[1].replace (")", "").strip()
+                check['endyear'] = check['startyear']
+                check['orderyear'] = check['endyear']
+            check['type'] = 'lost'
+            check['racestring'] = race
+            obj.append(check.copy())
+    # sort by the order year
+    newlist = sorted(obj, key=lambda k: k['orderyear'])
+    # return the reversed list
+    print newlist
+    return newlist[::-1]
+
 blueprint = Blueprint('add', __name__)
 
-@blueprint.route('/<namekey>.html')
-def person_detail(namekey):
+@blueprint.route('/candidate/<namekey>/')
+def persondetail(namekey):
     "Show details on a single id"
     # get context, also setting site.data
     site = g.current_site
     context = site.get_context()
     context.update({
         'PROJECT_PATH': site.path,
-        'PATH': '/<namekey>.html'
+        'PATH': '/candidate/<namekey>/'
     })
     info_by_name = build_dict(context['responses'], key="namekey")
-    context['selected'] = info_by_name[namekey]
-    context['selected']['education'] = build_edu(context['selected']['For_each_college_degree_you_have_list_the_degree_BA_PhD_the_field_of_study_and_the_college_or_university_Otherwise_please_provide_your_highest_level_of_education_and_the_school_you_attended'])
-    context['selected']['competitors'] = build_competition(context['candidates'],context['selected']['In_which_municipality_are_you_running_for_office'],context['selected']['Which_seat_are_you_running_for'],context['selected']['namekey'])
-
+    context['info_by_name'] = info_by_name
+    context['question_types'] = build_dict(context['questionlist'], key="question")
+    status_by_name = build_dict(context['candidates'], key="namekey")
+    context['selectedstatus'] = status_by_name[namekey]
+    if context['selectedstatus']['status'] == 'received':
+        context['selected'] = info_by_name[namekey]
+        context['selected']['previousraces'] = getraces(context['selected'])
+        context['selected']['education'] = build_edu(context['selected']['For_each_college_degree_you_have_list_the_degree_BA_PhD_the_field_of_study_and_the_college_or_university_Otherwise_please_provide_your_highest_level_of_education_and_the_school_you_attended'])
+    if context['selectedstatus']['status'] == 'pending':
+        context['selected'] = {}
+    context['selected']['questionlist'] = build_questions(context['questionlist'],context['selectedstatus']['city'])[context['selectedstatus']['city']]
+    context['selected']['competitors'] = build_competition(context['candidates'],context['selectedstatus']['city'],context['selectedstatus']['seat'],context['selectedstatus']['namekey'])
     return render_template('_person.html', **context)
 
 @blueprint.route('/')
@@ -91,6 +157,5 @@ def index():
         'PROJECT_PATH': site.path,
         'PATH': '/'
     })
-    # for row in context.cities:
-    #     context['cities'][row] = build_candidates(context['candidates'],row,seat)
+    context['cityseatlist'] = buildcityseatlist(context['candidates'])
     return render_template('index.html', **context)
